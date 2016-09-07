b75b03fe3d62abe3d7ef5cbc1e48d390cca683f8
RippleStudios <ruchir.bapat@gmail.com>
2016-09-08 00:00:42 +1000

    Updated SoundManager.cpp and fixed some errors.

--- Project/Engine/SoundManager.cpp
+++ Project/Engine/SoundManager.cpp
@@ -1,55 +1,94 @@
-/*
 #include "SoundManager.h"
 
 namespace SoundManager {
+	//The maximum number of buffers and audio sources our program will need
+#define MAX_AUDIO_BUFFERS 64
+#define MAX_AUDIO_SOURCES 16
 
-	bool isInit;
-	ALCdevice *Device;
-	ALCcontext *Context;
-	string AudioPath;
-	bool isSoundOn;
-	ALfloat Position[3];
-	ALfloat Velocity[3];
-	ALfloat Orientation[6];
+	//Useless typedef for samples in decoding ogg's
+	typedef short SAMPLES;
+	//Boolean for whether the SoundManager has been initialised
+	 bool isInit;
+	//The OpenAL Device
+	 ALCdevice *Device;
+	//The OpenAL Context
+	 ALCcontext *Context;
+	//Path at which the audio is found at
+	 string AudioPath;
+	//Boolean for whether sound is currently playing
+	 bool isSoundOn;
+	//Initial position of the listener
+	 ALfloat Position[3];
+	//Initial velocity of the listener
+	 ALfloat Velocity[3];
+	//Initial orientation of the listener, and holds 6 items: orientation + orientation's direction UP
+	 ALfloat Orientation[6];
 
-	unsigned int AudioSourcesInUseCount;
-	vector<ALuint> AudioSources;
-	vector<bool> AudioSourcesInUse;
+	 //How many audio sources are being used
+	  unsigned int AudioSourcesInUseCount;
+	 //Vector of all the audio sources
+	  ALuint AudioSources[MAX_AUDIO_SOURCES];
+	 //Vector of the audio sources of which are in use
+	  bool AudioSourcesInUse[MAX_AUDIO_SOURCES];
 
-	unsigned int AudioBuffersInUseCount;
-	vector<ALuint> AudioBuffers;
-	vector<bool> AudioBuffersInUse;
-	vector<string> AudioBufferFileName;
+	 //How many audio buffers are being used
+	  unsigned int AudioBuffersInUseCount;
+	 //Vector of all the audio buffers
+	  ALuint AudioBuffers[MAX_AUDIO_BUFFERS];
+	 //Vector of the audio buffers of which are in use
+	  bool AudioBuffersInUse[MAX_AUDIO_BUFFERS];
+	 //Holds the file names (paths) of a sound in the buffer
+	  char AudioBufferFileName[MAX_AUDIO_BUFFERS][120];
 
-	int LocateAudioBuffer(string path);
-	int LoadAudioToBuffer(string path);
-	bool LoadOGG(string path, ALuint destinationAudioBuffer);
+	//Function to locate whether a sound has already been loaded to a buffer
+	 int LocateAudioBuffer(string path);
+	//Functino to load audio into an OpenAL buffer
+	 int LoadAudioToBuffer(string path);
+	//Function to decode a .ogg file and put that into a buffer, takes in a destination buffer 
+	 bool LoadOGG(string path, ALuint destinationAudioBuffer);
+	//Incomplete function to load a .wav file and put that into a buffer, takes in a destination buffer
+	 bool LoadWAV(string path, ALuint destinationAudioBuffer);
 
-	void Init(Vec3 listener_pos, Vec3 listener_velocity, Vec3 listener_orientation_dir, Vec3 listener_orientation_dir_up);
-	void Kill();
+	//Initialises the SoundManager singleton class, takes the position, velocity and orientation of the listener
+	 void Init(Vec3 listener_pos, Vec3 listener_velocity, Vec3 listener_orientation_dir, Vec3 listener_orientation_dir_up);
+	//Destroys the SoundManager
+	 void Kill();
 
-	bool IsSoundOn();
-	void SetAudioPath(string path);
+	//Returns whether sound is playing
+	 bool IsSoundOn();
+	//Sets the audio path for where audio files are found
+	 void SetAudioPath(string path);
 
-	string ListAvailableDevices();
+	//Lists the available OpenAL devices
+	 string ListAvailableDevices();
 
-	bool LoadAudio(string path, unsigned int *audioID, bool loop);
-	bool ReleaseAudio(unsigned int audioID);
-	bool PlayAudio(unsigned int audioID, bool forceRestart);
-	bool StopAudio(unsigned int audioID);
-	bool StopAllAudio();
+	//Loads the audio, takes in the path at which the file is found, an uint for the ID and a bool for whether you want to loop the audio
+	 bool LoadAudio(string path, unsigned int *audioID, bool loop);
+	//Releases and deletes the audio from OpenAL memory
+	 bool ReleaseAudio(unsigned int audioID);
+	//Plays an audio, takes the audio to play and a bool for whether you want to start playing from the start, if set to false, it just continues from where it was paused
+	 bool PlayAudio(unsigned int audioID, bool forceRestart);
+	//Stops an audio, takes the audio to stop
+	 bool StopAudio(unsigned int audioID);
+	//Loops through all audio sources and stops them all
+	 bool StopAllAudio();
 
-	bool PauseAudio(unsigned int audioID);
-	bool PauseAllAudio();
-	bool ResumeAudio(unsigned int audioID);
-	bool ResumeAllAudio();
-	bool SetSoundPosition(unsigned int audioID, Vec3 position);
-	bool SetSoundPosition(unsigned int audioID, Vec3 position, Vec3 velocity, Vec3 direction);
-	bool SetSound(unsigned int audioID, Vec3 position, Vec3 velocity, Vec3 direction, float maxDistance, bool playNow, bool loop, float minGain);
-	bool SetListenerPosition(Vec3 position, Vec3 velocity, glm::quat orientation);
-
-
-
+	//Pauses the audio, takes the audio to be paused
+	 bool PauseAudio(unsigned int audioID);
+	//Loops through each audio source and pauses all of them
+	 bool PauseAllAudio();
+	//Resumes a specific audio, takes an audio to be resumed
+	 bool ResumeAudio(unsigned int audioID);
+	//Loops through each audio source and the resumes all of them
+	 bool ResumeAllAudio();
+	//Sets the audio source position for a certain audio, takes the audio, and a Vec3 for the position you want to move it to
+	 bool SetSoundPosition(unsigned int audioID, Vec3 position);
+	//Sets the audio source position for a certain audio, takes the audio, and a Vec3 for the position you want to move it to, the velocity of the source and the direction of the source
+	 bool SetSoundPosition(unsigned int audioID, Vec3 position, Vec3 velocity, Vec3 direction);
+	//Sets a audio source at a position, takes the sound and Vec3s for position, velocity, direction. Takes a float for the maximum distance from the listener it can be heard at, a boolean for whether to play the audio, a boolean for whether to loop the audio, and a float for the minimum gain of the audio emitted by the source
+	 bool SetSound(unsigned int audioID, Vec3 position, Vec3 velocity, Vec3 direction, float maxDistance, bool playNow, bool loop, float minGain);
+	//Sets the position of the OpenAL listener, takes in Vec3s for position, velocity and a GLM Quaternion for the orientation
+	 bool SetListenerPosition(Vec3 position, Vec3 velocity, glm::quat orientation);
 
 
 	void Init(Vec3 listener_pos, Vec3 listener_velocity, Vec3 listener_orientation_dir, Vec3 listener_orientation_dir_up)
@@ -92,13 +131,16 @@ namespace SoundManager {
 
 		Device = alcOpenDevice(0);
 		if (!Device) Console::error("OpenAL Device was not created!");
+		if (Device != nullptr) Console::message("OpenAL Device created.");
 		Context = alcCreateContext(Device, NULL);
 		if (!Context)Console::error("OpenAL Context was not created!");
+		if (Device != nullptr) Console::message("OpenAL Context created.");
 		alcMakeContextCurrent(Context);
 
-		for (int i = 0; i < MAX_AUDIO_BUFFERS; i++)
+		/*for (int i = 0; i < MAX_AUDIO_BUFFERS; i++)
 		{
 			ALuint bufferID = AudioBuffers[i];
+			strcpy(AudioBufferFileName[i], "--");
 			alGenBuffers(1, &bufferID);
 		}
 
@@ -106,8 +148,25 @@ namespace SoundManager {
 		{
 			ALuint sourceID = AudioSources[i];
 			alGenBuffers(1, &sourceID);
+		}*/
+
+		for (int i = 0; i < MAX_AUDIO_BUFFERS; i++)
+		{
+			AudioBuffers[i] = 0;
+			strcpy(AudioBufferFileName[i], "--");
+			AudioBuffersInUse[i] = false;
 		}
 
+		for (int i = 0; i < MAX_AUDIO_SOURCES; i++)
+		{
+			AudioSources[i] = 0;
+			AudioSourcesInUse[i] = false;
+		}
+
+		alGenBuffers(MAX_AUDIO_BUFFERS, AudioBuffers);
+
+		alGenSources(MAX_AUDIO_SOURCES, AudioSources);		
+		
 		alListenerfv(AL_POSITION, Position);
 
 		alListenerfv(AL_VELOCITY, Velocity);
@@ -127,14 +186,14 @@ namespace SoundManager {
 
 	void Kill()
 	{
-		for (int i = 0; i < AudioSources.size(); i++)
+		for (int i = 0; i < sizeof(AudioSources) / sizeof(AudioSources[0]); i++)
 		{
 			alDeleteSources(1, &AudioSources[i]);
 		}
 
-		for (int i = 0; i < AudioBuffers.size(); i++)
+		for (int i = 0; i < sizeof(AudioBuffers) / sizeof(AudioBuffers[0]); i++)
 		{
-			alDeleteSources(1, &AudioBuffers[i]);
+			alDeleteBuffers(1, &AudioBuffers[i]);
 		}
 
 		Context = alcGetCurrentContext();
@@ -163,51 +222,60 @@ namespace SoundManager {
 		}
 		return deviceList;
 	}
+bool LoadAudio(string path, unsigned int *audioID, bool loop){
+	if (path.empty() || path.length() > 120)
+		return false;
 
-	bool LoadAudio(string path, unsigned int *audioID, bool loop)
+	if (AudioSourcesInUseCount == MAX_AUDIO_SOURCES)
 	{
-		if (path.empty())
-			Console::warning("Audio not loaded. Filepath is empty."); return false;
-
-		if (AudioSourcesInUseCount == MAX_AUDIO_SOURCES)
-			Console::error("Out of Audio Source slots! Increase macro size in SoundManager.h to fit more!"); return false;
-
-		int bufferID, sourceID;
-		bufferID = sourceID = -1;
-		alGetError();
-
-		bufferID = LocateAudioBuffer(path);
-		if (bufferID < 0)
-		{
-			bufferID = LoadAudioToBuffer(path);
-			if (bufferID < 0)return false;
-		}
-
-		sourceID = 0;
-		while (AudioSourcesInUse[sourceID] == true)
-			sourceID++;
-
-		*audioID = sourceID;
-		AudioSourcesInUse[sourceID] = true;
-		AudioSourcesInUseCount++;
-
-		alSourcei(AudioSources[sourceID], AL_BUFFER, AudioBuffers[bufferID]);
-		alSourcei(AudioSources[sourceID], AL_LOOPING, ((loop == true) ? AL_TRUE : AL_FALSE));
-
-		return true;
+		Console::error("LoadAudio() : No more audio sources");
+		return false;   // out of Audio Source slots!
 	}
+
+	int bufferID = -1;   // Identity of the Sound Buffer to use
+	int sourceID = -1;   // Identity of the Source Buffer to use
+
+	alGetError();    // Clear Error Code
+
+					 // Check and see if the pSoundFile is already loaded into a buffer
+	bufferID = LocateAudioBuffer(path);
+	if (bufferID < 0)
+	{
+		// The sound file isn't loaded in a buffer, lets attempt to load it on the fly
+		bufferID = LoadAudioToBuffer(path);
+		if (bufferID < 0) return false;   // failed!
+	}
+
+	// If you are here, the sound the requester wants to reference is in a buffer.
+	// Now, we need to find a free Audio Source slot in the sound system
+	sourceID = 0;
+
+	while (AudioSourcesInUse[sourceID] == true) sourceID++;
+
+	// When you are here, 'mSourceID' now represents a free Audio Source slot
+	// The free slot may not be at the end of the array but in the middle of it.
+	*audioID = sourceID;  // return the Audio Source ID to the caller
+	AudioSourcesInUse[sourceID] = true; // mark this Source slot as in use
+	AudioSourcesInUseCount++;    // bump the 'in use' counter
+
+								  // Now inform OpenAL of the sound assignment and attach the audio buffer
+								  // to the audio source
+	alSourcei(AudioSources[sourceID], AL_BUFFER, AudioBuffers[bufferID]);
+
+	// Steven : Not in the original code !!!!!
+	alSourcei(AudioSources[sourceID], AL_LOOPING, loop);
+
+	return true;
+ }
+
 
 	int LocateAudioBuffer(string path)
 	{
-		for (unsigned int i = 0; i < MAX_AUDIO_BUFFERS; i++)
+		for (unsigned int b = 0; b < MAX_AUDIO_BUFFERS; b++)
 		{
-			if (path == AudioBufferFileName[i])
-			{
-				return (int)i;
-			}
+			if (path == AudioBufferFileName[b]) return (int)b; // TODO Careful : converts unsigned to int!
 		}
-		Console::message("File does not already exist in OpenAL buffer");
-		return -1;
+		return -1;      // not found
 	}
 
 	int LoadAudioToBuffer(string path)
@@ -219,7 +287,7 @@ namespace SoundManager {
 
 		if (AudioBuffersInUseCount == MAX_AUDIO_BUFFERS)
 		{
-			Console::warning("No more available slots for OpenAL Audio Buffers! Increase macro value in SoundManager.h for more slots!");
+			Console::warning("LoadAudioToBuffer() : No more available slots for OpenAL Audio Buffers! Increase macro value in SoundManager.h for more slots!");
 			return -1;
 		}
 
@@ -238,34 +306,37 @@ namespace SoundManager {
 		}
 
 		AudioBuffersInUse[bufferID] = true;
-		AudioBufferFileName[bufferID] = path;
+		strcpy(AudioBufferFileName[bufferID], path.c_str());
 		AudioBuffersInUseCount++;
 		return bufferID;
 	}
 
-	bool PlayAudio(unsigned int audioID, bool forceRestart)
+	bool SoundManager::PlayAudio(unsigned int audioID, bool forceRestart)
 	{
-		if (audioID >= MAX_AUDIO_SOURCES || AudioSourcesInUse[audioID])
+		// Make sure the audio source ident is valid and usable
+		if (audioID >= MAX_AUDIO_SOURCES || !AudioSourcesInUse[audioID])
 		{
-			Console::error("Audio ID is not valid and usable OR there are no more audio sources available. Increase macro value in SoundManager.h for more slots!");
+			Console::error("PlayAudio() : No more available slots for OpenAL Audio Buffers! Increase macro value in SoundManager.h for more slots!");
 			return false;
 		}
+
 		int sourceAudioState = 0;
+
 		alGetError();
+
+		// Are we currently playing the audio source?
 		alGetSourcei(AudioSources[audioID], AL_SOURCE_STATE, &sourceAudioState);
+
 		if (sourceAudioState == AL_PLAYING)
 		{
 			if (forceRestart)
-			{
 				StopAudio(audioID);
-			}
 			else
-			{
-				return false;
-			}
+				return false; // Not forced, so we don't do anything
 		}
 
 		alSourcePlay(AudioSources[audioID]);
+		cout << "aID" << audioID << endl;
 		return true;
 	}
 
@@ -289,7 +360,7 @@ namespace SoundManager {
 		}
 		alGetError();
 
-		for (int i = 0; i < AudioSources.size(); i++)
+		for (int i = 0; i < sizeof(AudioSources) / sizeof(AudioSources[0]); i++)
 		{
 			alSourcePause(AudioSources[i]);
 		}
@@ -301,7 +372,7 @@ namespace SoundManager {
 	{
 		if (audioID >= MAX_AUDIO_SOURCES || !AudioSourcesInUse[audioID])
 		{
-			Console::error("Audio ID is not valid and usable OR there are no more audio sources available. Increase macro value in SoundManager.h for more slots!");
+			Console::error("ResumeAudio() : Audio ID is not valid and usable OR there are no more audio sources available. Increase macro value in SoundManager.h for more slots!");
 			return false;
 		}
 
@@ -316,7 +387,7 @@ namespace SoundManager {
 	{
 		if (AudioSourcesInUseCount >= MAX_AUDIO_SOURCES)
 		{
-			Console::warning("No more audio source slots available. Increase macro value in SoundManager.h for more slots!");
+			Console::warning("ResumeAllAudio() : No more audio source slots available. Increase macro value in SoundManager.h for more slots!");
 			return false;
 		}
 
@@ -340,7 +411,7 @@ namespace SoundManager {
 	{
 		if (audioID >= MAX_AUDIO_SOURCES || !AudioSourcesInUse[audioID])
 		{
-			Console::error("Audio ID is not valid and usable OR there are no more audio sources available. Increase macro value in SoundManager.h for more slots!");
+			Console::error("StopAudio() : Audio ID is not valid and usable OR there are no more audio sources available. Increase macro value in SoundManager.h for more slots!");
 			return false;
 		}
 
@@ -355,7 +426,7 @@ namespace SoundManager {
 	{
 		if (AudioSourcesInUseCount >= MAX_AUDIO_SOURCES)
 		{
-			Console::warning("No more audio source slots available. Increase macro value in SoundManager.h for more slots!");
+			Console::warning("StopAllAudio() : No more audio source slots available. Increase macro value in SoundManager.h for more slots!");
 			return false;
 		}
 
@@ -373,7 +444,7 @@ namespace SoundManager {
 	{
 		if (audioID >= MAX_AUDIO_SOURCES)
 		{
-			Console::error("Audio ID is not valid and usable OR there are no more audio sources available. Increase macro value in SoundManager.h for more slots!");
+			Console::error("ReleaseAudio() : Audio ID is not valid and usable OR there are no more audio sources available. Increase macro value in SoundManager.h for more slots!");
 			return false;
 		}
 		alGetError();
@@ -557,7 +628,7 @@ namespace SoundManager {
 			}
 		}
 
-		alBufferData(destinationAudioBuffer, format, &samples[0], ov_pcm_total(&oggfile, -1), fileInformation->rate);
+		alBufferData(destinationAudioBuffer, format, &samples[0], (ALsizei)ov_pcm_total(&oggfile, -1), fileInformation->rate);
 
 		return true;
 
@@ -573,4 +644,4 @@ namespace SoundManager {
 		AudioPath = path;
 	}
 
-} */
+}

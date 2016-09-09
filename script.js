a86bd966b59cfc751f685342f4f53b000779613b
Obtuse Studios <TheDesignerPenguin@gmail.com>
2016-09-09 10:50:19 +1000

    Added the basic website.

---
+++ script.js
@@ -0,0 +1,12 @@
+//Set full page properties
+fullpage.initialize('#fullpage', {
+    anchors: ['firstPage', 'secondPage'],
+    navigation: true,
+    scrollingSpeed: 600,
+    menu: '#menu',
+    verticalCentered: false,
+    navigationPosition: 'right',
+    navigationTooltips: ['Home', 'Game']
+});
+
+function main() { }

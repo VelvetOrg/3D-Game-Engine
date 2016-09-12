74dd390b051d89325bf76a7a5d4924433a379648
RippleStudios <ruchir.bapat@gmail.com>
2016-09-12 10:05:08 +1000

    Commented SoundManager.h
    Commented the SoundManager header file for improved readability.

--- Project/Engine/SoundManager.h
+++ Project/Engine/SoundManager.h
@@ -1,15 +1,17 @@
+//Include guards
 #pragma once
-#include <ambiera\irrKlang.h>
-#include "Console.h"
-
+//Includes
+#include <ambiera\irrKlang.h> // irrKlang
+#include "Console.h" // Console printing
+#include <glm\glm.hpp> // To use GLM Vector3 class
 using namespace irrklang;
 namespace SoundManager
 {
-	extern void Init();
-	extern void Play(const char* path, bool loop);
-	extern void Play(const char* path, bool loop, vec3df position);
-	extern void Pause();
-	extern void Resume();
-	extern void Kill();
-	extern ISoundEngine *Engine;
+	extern void Init(); // Initialises the SoundManager and creates a SoundEngine
+	extern void Play(const char* path, bool loop); // Plays a 2D Sound, takes in the path and whether to loop the audio
+	extern void Play(const char* path, bool loop, glm::vec3 position); // Plays a 3D Sound, takes in the path, whether the loop the // audio and a vector to play the sound at
+	extern void Pause(); // Pauses an audio. Incomplete.
+	extern void Resume(); // Resumes an audio. Incomplete.
+	extern void Kill(); // Destroys and cleans up the SoundManager
+	extern ISoundEngine *Engine; // The ISoundEngine
 }

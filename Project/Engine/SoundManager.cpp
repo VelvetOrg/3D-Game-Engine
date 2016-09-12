ca11db44f724ff063fe0c4f2f3fa3be29ab75fbe
RippleStudios <ruchir.bapat@gmail.com>
2016-09-12 10:08:05 +1000

    Changed SoundManager to use GLM Vector3
    Changed the SoundManager's Play function (for 3D Sound) to use the GLM Vector3 class instead of irrKlang's own vec3df. Also commented the SoundManager .cpp file for improved readability.

--- Project/Engine/SoundManager.cpp
+++ Project/Engine/SoundManager.cpp
@@ -1,40 +1,48 @@
 #include "SoundManager.h"
 namespace SoundManager
 {
+    //Functions re-declared
 	void Init();
 	void Play(const char* path, bool loop);
-	void Play(const char* path, bool loop, vec3df position);
+	void Play(const char* path, bool loop, glm::vec3 position);
 	void Pause();
 	void Resume();
 	void Kill();
+    //Engine re-declared
 	ISoundEngine *Engine;
 
 	void Init()
 	{
+        //Create irrKlangDevice
 		Engine = createIrrKlangDevice();
 	}
 
 	void Play(const char* path, bool loop)
 	{
+        //Plays the sound with the Engine
 		Engine->play2D(path, loop);
 	}
 
-	void Play(const char* path, bool loop, vec3df position)
+	void Play(const char* path, bool loop, glm::vec3 position)
 	{
-		Engine->play3D(path, position, loop);
+        //Convert glm::vec3 to vec3df (used by irrKlang)
+        vec3df tempPos = vec3df(position.x, position.y, position.z);
+        //Plays the sound with the Engine
+		Engine->play3D(path, tempPos, loop);
 	}
 
 	void Pause()
 	{
+        //Should pause the sound. Incomplete.   
 	}
 
 	void Resume()
 	{
-
+        //Should resume the sound. Incomplete.
 	}
 
 	void Kill()
 	{
-
+        //Should destroy and kill the SoundManager. Incomplete.
 	}
 }

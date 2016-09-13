9f5444ebfdf8f3f037bfd36608c5ba2392aa28cf
RippleStudios <ruchir.bapat@gmail.com>
2016-09-13 12:21:31 +1000

    Updated Physics.cpp

--- Project/Engine/Physics.cpp
+++ Project/Engine/Physics.cpp
@@ -1,1 +1,19 @@
 #include "Physics.h"
+
+namespace Physics
+{
+  //Sets Bullet up
+  void Init();
+  void Init()
+  {
+    //Defines Bullet's algorith for objects that should not collide
+    btBroadPhaseInterface *broadPhase = new btDbvtBroadphase();
+    //Default Bullet collision algorithm
+    btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
+    btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
+    //Incorporates gravity, collisions and object interactions
+    btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
+    //Create dynamic world
+    btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher,broadphase,solver,collisionConfiguration);
+  }
+}
